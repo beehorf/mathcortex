d4dcb4e511aeee6346b92df55c7f2881d68cf31d
beehorf <agencay@yahoo.com>
2015-12-07 17:18:17 +0200

    Update cortex_parser.js

--- cortex_parser.js
+++ cortex_parser.js
@@ -3,18 +3,18 @@ Compiler for MathCortex language
 
 Copyright (c) 2012-2015 Gorkem Gencay. 
 
-MathCortex compiler is free software: you can redistribute it and/or modify
+MathCortex Compiler is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Foobar is distributed in the hope that it will be useful,
+MathCortex Compiler is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+along with MathCortex Compiler.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 "use strict";
